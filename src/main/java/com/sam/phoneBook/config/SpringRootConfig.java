7ef986415ee94ee01a047f45c8ec8007253736d9
future-pirate-king <samd948@gmail.com>
2018-12-16 16:43:02 +0530

    Initial Commit

---
+++ src/main/java/com/sam/phoneBook/config/SpringRootConfig.java
@@ -0,0 +1,11 @@
+package com.sam.phoneBook.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.sam.phoneBook"})
+public class SpringRootConfig {
+
+	
+}
